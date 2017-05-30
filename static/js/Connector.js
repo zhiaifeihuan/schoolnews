ee44e70e1ffffe38fe096c0d523668f4d87644ab
zhiaifeihuan <zhiaifeihuan@qq.com>
2017-05-31 00:54:07 +0800

    aaa

---
+++ static/js/Connector.js
@@ -0,0 +1,35 @@
+/**
+ * 包装jquery的ajax请求，
+ * @param {type} opts 
+ * {
+ *      url: "",
+ *      params: {},
+ *      success:function(json){},
+ *      failure:function(json){}
+ * }
+ * @returns {void}
+ */
+import jquery from 'jquery'
+export default function (opts) {
+    jquery.ajax({
+        url: opts.url,
+        data: {
+            request: JSON.stringify(opts.params)
+        },
+        method: "POST",
+        success: function (data) {
+            var json = JSON.parse(data);
+            if (json.success && opts.success && typeof opts.success === "function") {
+                opts.success(json);
+            } else if (!json.success && opts.failure && typeof opts.failure === "function") {
+                opts.failure(json);
+            } else {
+                alert(json.message);
+            }
+        },
+        error: function (data) {
+            alert("网络错误，请检查您的网络连接!");
+        }
+    });
+};
+
