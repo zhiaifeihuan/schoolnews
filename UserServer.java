8bdb174d2b79966b30ad281e1a70af9689e104e2
mwd <2454616590@qq.com>
2017-06-08 22:18:07 +0800

    dsd

--- UserServer.java
+++ UserServer.java
@@ -222,16 +222,37 @@ public class UserServer extends Server{
     
     private void alter(){
         try{
+             HttpSession session = request.getSession(false);
+             String curusername = (String) session.getAttribute("HASLOGIN");
              String sql1 ="select count(1) from sn_user where nickname=? ";
              PreparedStatement st1 = connection.prepareStatement(sql1);
              st1.setString(1, data.getString("nickname"));
              ResultSet rs1 = st1.executeQuery();
              rs1.next();
              if(rs1.getInt(1) == 1){
-                this.makeResponse(false, "昵称已存在!", null);
+                 String sql2 ="select username from sn_user where nickname = ?";
+                 PreparedStatement st2 = connection.prepareStatement(sql2);
+                 st2.setString(1, data.getString("nickname"));
+                 ResultSet rs2 = st2.executeQuery();
+                 rs2.next();
+                 String username = rs2.getString(1);
+                 if(username.equals (curusername)){
+                     String sql = "update sn_user set gender = ?,introduce = ?,identity = ?,major = ?,phone = ?,nickname = ? where username = ?";
+                     PreparedStatement st = connection.prepareStatement(sql);
+                     st.setString(1, data.getString("gender"));
+                     st.setString(2, data.getString("introduce"));
+                     st.setString(3, data.getString("identity"));
+                     st.setString(4, data.getString("major"));
+                     st.setString(5, data.getString("phone"));
+                     st.setString(6, data.getString("nickname"));
+                     st.setString(7, curusername);
+                     int rs = st.executeUpdate();
+                 this.makeResponse(true, "修改个人信息成功!", null);}
+                 else{
+                  this.makeResponse(false, "昵称已存在!", null); 
+                 }
             }else{
-             HttpSession session = request.getSession(false);
-             String curusername = (String) session.getAttribute("HASLOGIN");
+             
              String sql = "update sn_user set gender = ?,introduce = ?,identity = ?,major = ?,phone = ?,nickname = ? where username = ?";
              PreparedStatement st = connection.prepareStatement(sql);
              st.setString(1, data.getString("gender"));
